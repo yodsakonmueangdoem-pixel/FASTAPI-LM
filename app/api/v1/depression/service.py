3fb5072975ec35b8b5ef045a59ff01904e50d4fe
siwapan-imac <work.siwapun.kh@gmail.com>
2026-01-23 20:23:52 +0700

    Refactor depression prediction logic to load model within the function and update DataFrame conversion

--- app/api/v1/depression/service.py
+++ app/api/v1/depression/service.py
@@ -7,9 +7,9 @@ from loguru import logger
 MODEL_PATH = (
     Path(__file__).parent.parent.parent.parent / "models" / "depression_model.joblib"
 )
-artifact = joblib.load(MODEL_PATH)
 
-def to_df(req: DepressionRequest) -> pd.DataFrame:
+
+def to_df(req: DepressionRequest, features) -> pd.DataFrame:
     row = {
         "Gender": req.Gender,
         "Age": req.Age,
@@ -25,20 +25,18 @@ def to_df(req: DepressionRequest) -> pd.DataFrame:
 
     
    
-    features = artifact["features"]
     return pd.DataFrame([row], columns=features)
 
 
 def predict_depression(request: DepressionRequest) -> DepressionResponse:
     logger.info(f"Load model successfully from {MODEL_PATH}")
+    artifact = joblib.load(MODEL_PATH)
     
-    X = to_df(request)
+    X = to_df(request,artifact["features"])
     model = artifact["model"]
 
     prob = float(model.predict_proba(X)[0][1])
     pred = int(model.predict(X)[0])
-
-    model = joblib.load(MODEL_PATH)
 
     return DepressionResponse(
         prediction="Yes" if pred == 1 else "No",
